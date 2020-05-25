b4b8cad7fb3c10924fde792eb4ef074379fd4d73
Bruno Mello <bruno.barros.mello@gmail.com>
2020-05-24 22:54:28 -0300

    BM: prettier config and integrate with eslint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (req, res) => res.send({ ok: true }));
+routes.get('/', (request, response) =>
+  response.send({ message: 'Hello GoStack' }),
+);
 
 export default routes;
