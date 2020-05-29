3644ea9275a6280ebae32b6db14ccb0cdbc194f9
Bruno Mello <bruno.barros.mello@gmail.com>
2020-05-29 19:27:51 -0300

    BM: change appointments and users id type to uuid

--- src/database/migrations/1590378116490-CreateAppointments.ts
+++ src/database/migrations/1590378116490-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1590378116490
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
