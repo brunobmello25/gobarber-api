b3c7ed0cbf34576c75d863ac1ba83866979ea286
Bruno Mello <bruno.barros.mello@gmail.com>
2020-06-05 20:54:59 -0300

    BM: add mock users repository

---
+++ src/tests/users/mocks/MockUsersRepository.ts
@@ -0,0 +1,32 @@
+import { uuid } from 'uuidv4';
+
+import { User } from '@modules/users/infra/typeorm/entities';
+import { IUsersRepository } from '@modules/users/repositories';
+
+class UsersRepository implements IUsersRepository {
+  private repository: User[] = [];
+
+  public async create(data: ICreateUserDTO): Promise<User> {
+    const user = new User();
+    Object.assign(user, { id: uuid(), ...data });
+
+    this.repository.push(user);
+    return user;
+  }
+
+  public async findById(id: string): Promise<User | undefined> {
+    return this.repository.find((u) => u.id === id);
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    return this.repository.find((u) => u.email === email);
+  }
+
+  public async save(user: User): Promise<User> {
+    const index = this.repository.findIndex((u) => u.id === user.id);
+    this.repository[index] = user;
+    return user;
+  }
+}
+
+export default UsersRepository;
