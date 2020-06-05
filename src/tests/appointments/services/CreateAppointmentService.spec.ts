a61a99d853f7b35de177f1fd238066dfe72f7919
Bruno Mello <bruno.barros.mello@gmail.com>
2020-06-05 19:51:02 -0300

    BM: move create appointments test to tests folder

---
+++ src/tests/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,7 @@
+import { CreateAppointmentService } from '@modules/appointments/services';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {});
+
+  // it('should not be able to create two appointments on the same time', () => {});
+});
