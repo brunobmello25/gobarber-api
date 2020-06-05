222aa001632025a738aeacde3b58bffd9e23c85b
Bruno Mello <bruno.barros.mello@gmail.com>
2020-06-05 20:34:13 -0300

    BM: add test create a new appointment

--- src/tests/appointments/services/CreateAppointmentService.spec.ts
+++ src/tests/appointments/services/CreateAppointmentService.spec.ts
@@ -1,7 +1,17 @@
+import { MockAppointmentsRepository } from '@tests/appointments/mocks';
 import { CreateAppointmentService } from '@modules/appointments/services';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {});
+  it('should be able to create a new appointment', async () => {
+    const mockAppointmentsRepository = new MockAppointmentsRepository();
+
+    const appointment = await new CreateAppointmentService(
+      mockAppointmentsRepository,
+    ).execute({ date: new Date(), providerId: '123123' });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.providerId).toBe('123123');
+  });
 
   // it('should not be able to create two appointments on the same time', () => {});
 });
