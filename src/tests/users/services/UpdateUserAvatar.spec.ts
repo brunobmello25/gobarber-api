c8c03094dbf571a0822b4341bbfd66b2c506c3ff
Bruno Mello <bruno.barros.mello@gmail.com>
2020-06-05 22:17:57 -0300

    BM: add test for updating avatar on non existing user

--- src/tests/users/services/UpdateUserAvatar.spec.ts
+++ src/tests/users/services/UpdateUserAvatar.spec.ts
@@ -1,6 +1,7 @@
 import { MockUsersRepository } from '@tests/users/mocks';
 import { UpdateUserAvatarService } from '@modules/users/services';
 import MockStorageProvider from '@shared/providers/StorageProvider/mocks/MockStorageProvider';
+import { ApplicationError } from '@shared/errors';
 
 describe('UpdateUserAvatar', () => {
   it('should be able update user avatar', async () => {
@@ -19,5 +20,17 @@ describe('UpdateUserAvatar', () => {
     ).execute({ userId: user.id, avatarFilename: 'avatar.jpg' });
 
     expect(user.avatar).toBe('avatar.jpg');
+  });
+
+  it('should be able update user avatar', async () => {
+    const mockUsersRepository = new MockUsersRepository();
+    const mockStorageProvider = new MockStorageProvider();
+
+    expect(
+      new UpdateUserAvatarService(
+        mockUsersRepository,
+        mockStorageProvider,
+      ).execute({ userId: 'non-existing-user', avatarFilename: 'avatar.jpg' }),
+    ).rejects.toBeInstanceOf(ApplicationError);
   });
 });
