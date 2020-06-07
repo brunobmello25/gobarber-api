3fd0045b3dfad9f18820a07ee283ebb527f661aa
Bruno Mello <bruno.barros.mello@gmail.com>
2020-06-06 22:30:58 -0300

    BM: add test and feature for password retrieving with non existing email

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 
 import { IUsersRepository } from '@modules/users/repositories';
 import { IMailProvider } from '@shared/providers/MailProvider/models';
+import { ApplicationError } from '@shared/errors';
 
 interface IRequest {
   email: string;
@@ -15,6 +16,10 @@ class SendForgotPasswordEmailService {
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) throw new ApplicationError('User does not exist');
+
     this.mailProvider.sendEmail(
       email,
       'Pedido de recuperação de senha recebido',
--- src/tests/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ src/tests/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -1,6 +1,7 @@
 import { MockUsersRepository } from '@tests/modules/users/mocks';
 import { MockMailProvider } from '@shared/providers/MailProvider/mocks';
 import { SendForgotPasswordEmailService } from '@modules/users/services';
+import { ApplicationError } from '@shared/errors';
 
 describe('SendForgotPasswordEmail', () => {
   it('should be able to recover the password using the email', async () => {
@@ -23,5 +24,17 @@ describe('SendForgotPasswordEmail', () => {
     });
 
     expect(sendEmailFunction).toHaveBeenCalled();
+  });
+
+  it('should not be able to recover password for a non existing user', async () => {
+    const mockUsersRepository = new MockUsersRepository();
+    const mockMailProvider = new MockMailProvider();
+
+    await expect(
+      new SendForgotPasswordEmailService(
+        mockUsersRepository,
+        mockMailProvider,
+      ).execute({ email: 'user@email.com' }),
+    ).rejects.toBeInstanceOf(ApplicationError);
   });
 });
