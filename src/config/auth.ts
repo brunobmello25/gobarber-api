cb0222b60fb81c704400f9d2afe4962be178dc28
Bruno Mello <bruno.barros.mello@gmail.com>
2020-06-27 17:44:06 -0300

    BM: change app secret + remove old from auth.ts and get it from .env

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,7 +1,6 @@
 export default {
   jwt: {
-    // TODO: change jwt sign private key and move it to a secret config file
-    secret: 'e0ab2eeba535bc7aede62c46ee19ae33',
+    secret: process.env.APP_SECRET,
     expiresIn: '1d',
   },
 };
