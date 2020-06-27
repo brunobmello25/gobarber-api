82db7e8df97e97753570816fd131ca168daf5fb1
Bruno Mello <bruno.barros.mello@gmail.com>
2020-06-27 20:24:00 -0300

    BM: remove date iso parsing on appointments controller

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 
 import { CreateAppointmentService } from '@modules/appointments/services';
@@ -9,12 +8,10 @@ class AppointmentsController {
     const { providerId, date } = request.body;
     const userId = request.user.id;
 
-    const parsedDate = parseISO(date);
-
     const appointment = await container
       .resolve(CreateAppointmentService)
       .execute({
-        date: parsedDate,
+        date,
         providerId,
         userId,
       });
