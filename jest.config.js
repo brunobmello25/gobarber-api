9529c5ba39c993a50dd59c859571ab19d74d0136
Bruno Mello <bruno.barros.mello@gmail.com>
2020-06-27 19:03:39 -0300

    BM: config dotnev on jest config and fix auth jwt secret config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
+require('dotenv').config();
+
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
 const { compilerOptions } = require('./tsconfig');
 
