b2951350f7204fd964bfa858b0c907bf0d838b8b
Bruno Mello <bruno.barros.mello@gmail.com>
2020-09-20 15:34:53 -0300

    fix: use class transformer on providers controller

--- src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import { ListProvidersService } from '@modules/appointments/services';
 
@@ -11,7 +12,7 @@ class ProvidersController {
       userId,
     });
 
-    return response.json({ providers });
+    return response.json({ providers: classToClass(providers) });
   }
 }
 
