7421944eb247f33628fe4cd7ec7f3d4a944c1e15
Bruno Mello <bruno.barros.mello@gmail.com>
2020-09-19 21:24:20 -0300

    fix: using request query on providerAppointmentsController

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -6,14 +6,14 @@ import { ListProviderAppointmentsService } from '@modules/appointments/services'
 class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const providerId = request.user.id;
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
 
     const appointments = await container
       .resolve(ListProviderAppointmentsService)
       .execute({
-        day,
-        month,
-        year,
+        day: Number(day),
+        month: Number(month),
+        year: Number(year),
         providerId,
       });
 
